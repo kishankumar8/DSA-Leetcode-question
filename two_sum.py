a590573a81fc3b87e14b34bedb33b5c0a6334d63
kishan kumar <kt2561060@gmail.com>
2026-07-26 14:34:54 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSum(self, nums, target
          hashmap={}
          for i in range(len(nums)):
                 complement=target-nums[i]
