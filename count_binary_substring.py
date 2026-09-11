e2b6ce9b635fb9fc1414a1b1742818a59636967a
kishan kumar <kt2561060@gmail.com>
2026-09-11 10:11:59 +0530

    Add files via upload

---
+++ count_binary_substring.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        prev = 0
+        curr = 1
+        ans = 0
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                curr += 1
+            else:
+                ans += min(prev, curr)
+                prev = curr
+                curr = 1
+
+        ans += min(prev, curr)
+
+        return ans
